e84be04549746aba835421b2594bd87c6e5f665b
Jeremy Dorn <jeremy@jeremydorn.com>
2022-01-17 15:54:25 -0600

    Add feature flags support.
    TODO:
    - [x] Initial implementation
    - [ ] Import JSON unit test suite
    - [ ] Fix bugs discovered by test suite...

---
+++ lib/growthbook/mongrule.rb
@@ -0,0 +1,158 @@
+require 'json'
+
+module Growthbook
+  class Mongrule
+    def self.evalCondition(attributes, condition)
+      if condition.has_key?("$or")
+        return self.evalOr(attributes, condition["$or"])
+      end
+      if condition.has_key?("$nor")
+        return !self.evalOr(attributes, condition["$nor"])
+      end
+      if condition.has_key?("$and")
+        return self.evalAnd(attributes, condition["$and"])
+      end
+      if condition.has_key?("$not")
+        return !self.evalCondition(attributes, condition["$not"])
+      end
+
+      condition.each do |key, value|
+        if !self.evalConditionValue(value, self.getPath(attributes, key))
+          return false
+        end
+      end
+
+      return true
+    end
+
+    private
+
+    def self.evalOr(attributes, conditions)
+      return true if !conditions.length
+      conditions.each do |condition|
+        if self.evalCondition(attributes, condition)
+          return true
+        end
+      end
+      return false
+    end
+
+    def self.evalAnd(attributes, conditions)
+      conditions.each do |condition|
+        if !self.evalCondition(attributes, condition)
+          return false
+        end
+      end
+      return true
+    end
+
+    def self.isOperatorObject(obj)
+      obj.each do |key, value|
+        return false if key[0] != "$"
+      end
+      return true
+    end
+
+    def self.getType(attributeValue)
+      return "string" if attributeValue.is_a? String
+      return "number" if attributeValue.is_a? Integer
+      return "number" if attributeValue.is_a? Float
+      return "boolean" if attributeValue.is_a? Boolean
+      return "array" if attributeValue.is_a? Array
+      return "null" if attributeValue == nil
+      return "object"
+    end
+
+    def getPaths(attributes, path)
+      parts = path.split(".")
+      current = attributes
+
+      parts.each do |i, value|
+        if current.has_key?(value)
+          current = current[value]
+        else
+          return nil
+        end
+      end
+
+      return current
+    end
+
+    def evalConditionValue(conditionValue, attributeValue)
+      if conditionValue.is_a?(Hash) && self.isOperatorObject(conditionValue)
+        conditionValue.each do |key, value|
+          return false if !self.evalOperatorCondition(key, attributeValue, value)
+        end
+        return true
+      end
+      return conditionValue.to_json == attributeValue.to_json
+    end
+
+    def elemMatch(condition, attributeValue)
+      return false if !(attributeValue.is_a? Array)
+
+      attributeValue.each do |item|
+        if self.isOperatorObject(condition)
+          return true if self.evalConditionValue(condition, item)
+        else
+          return true if self.evalCondition(item, condition)
+        end
+      end
+      return false
+    end
+
+    def evalOperatorCondition(operator, attributeValue, conditionValue)
+      case operator
+      when "$eq"
+        return attributeValue == conditionValue
+      when "$ne"
+        return attributeValue != conditionValue
+      when "$lt"
+        return attributeValue < conditionValue
+      when "$lte"
+        return attributeValue <= conditionValue
+      when "$gt"
+        return attributeValue > conditionValue
+      when "$gte"
+        return attributeValue >= conditionValue
+      when "$regex"
+        re = Regexp.new(conditionValue)
+        return !!attributeValue.match(re)
+      when "$in"
+        return conditionValue.include? attributeValue
+      when "$nin"
+        return !conditionValue.include? attributeValue
+      when "$elemMatch"
+        return self.elemMatch(conditionValue, attributeValue)
+      when "$size"
+        return false if !(attributeValue.is_a? Array)
+        return self.evalConditionValue(conditionValue, attributeValue.length)
+      when "$all"
+        return false if !(attributeValue.is_a? Array)
+        conditionValue.each do |condition|
+          passed = false
+          attributeValue.each do |attr|
+            if evalConditionValue(condition, attr)
+              passed = true
+            end
+          end
+          return false if !passed
+        end
+        return true
+      when "$exists"
+        exists = (attributeValue != nil)
+        if !conditionValue
+          return !exists
+        else
+          return exists
+        end
+      when "$type"
+        return conditionValue == self.getType(attributeValue)
+      when "$not"
+        return !self.evalConditionValue(conditionValue, attributeValue)
+      else
+        return false
+      end
+    end
+  end
+end
--- lib/growthbook/util.rb
+++ lib/growthbook/util.rb
@@ -1,4 +1,5 @@
 require "fnv"
+require "uri"
 
 module Growthbook
   class Util
@@ -25,27 +26,88 @@ module Growthbook
       end
     end
 
-    def self.chooseVariation(userId, experiment)
-      testId = experiment.id
-      weights = experiment.getScaledWeights()
+    # Hash a string to a float between 0 and 1
+    def self.hash(str)
+      return (FNV.new.fnv1a_32(str)%1000)/1000.0
+    end
 
-      # Hash the user id and testName to a number from 0 to 1
-      n = (FNV.new.fnv1a_32(userId + testId)%1000)/1000.0
+    # Determine if hash is within a namespace
+    def self.inNamespace(hashValue, namespace)
+      n = self.hash(hashValue + "__" + namespace[0])
+      return n >= namespace[1] && n < namespace[2]
+    end
 
-      cumulativeWeight = 0
+    # Create an array of size n with equal values that sum to 1
+    def self.getEqualWeights(n)
+      weights = []
+      for i in 1..n
+        weights << (1.0 / n)
+      end
+      return weights
+    end
 
-      match = -1
-      i = 0
-      weights.each do |weight|
-        cumulativeWeight += weight
-        if n < cumulativeWeight
-          match = i
-          break
-        end
-        i+=1
+    # Determine bucket ranges for experiment variations
+    def self.getBucketRanges(numVariations, coverage = 1, weights = [])
+      # Make sure coverage is within bounds
+      coverage = 0 if coverage < 0
+      coverage = 1 if coverage > 1
+
+      # Default to equal weights
+      if weights.length != numVariations
+        weights = self.getEqualWeights(numVariations)
       end
 
-      return match
+      # If weights don't add up to 1 (or close to it), default to equal weights
+      total = weights.sum
+      if total < 0.99 || total > 1.01
+        weights = self.getEqualWeights(numVariations)
+      end
+
+      # Convert weights to ranges
+      cumulative = 0
+      ranges = []
+      weights.each do |w|
+        start = cumulative
+        cumulative = cumulative + w
+        ranges << [start, start + coverage * w]
+      end
+
+      return ranges
+    end
+
+    # Chose a variation based on a hash and range
+    def self.chooseVariation(n, ranges)
+      for i in 0..ranges.length
+        if n >= ranges[i][0] && n < ranges[i][1]
+          return i
+        end
+      end
+      return -1
+    end
+
+    # Get an override variation from a url querystring
+    # e.g. http://localhost?my-test=1 will return `1` for id `my-test`
+    def self.getQueryStringOverride(id, url, numVariations)
+      # Skip if url is empty
+      return nil if url == ""
+
+      # Parse out the query string
+      parsed = URI(url)
+      qs = URI.decode_www_form(parsed.query)
+
+      # Look for `id` in the querystring and get the value
+      val = qs.assoc(id).last
+      return nil if !val
+
+      # Parse the value as an integer
+      n = Integer(val) rescue nil
+
+      # Make sure the integer is within range
+      return nil if n = nil
+      return nil if n < 0
+      return nil if n >= numVariations
+
+      return n
     end
   end
 end
