7f5c20bebf3777e4ddcd3ea44b6bb7d29fce06cf
tinahollygb <tina@growthbook.io>
2023-04-18 20:10:23 -0700

    fix invalid returns label

--- lib/growthbook/inline_experiment.rb
+++ lib/growthbook/inline_experiment.rb
@@ -3,52 +3,52 @@
 module Growthbook
   # Class for creating an inline experiment for evaluating
   class InlineExperiment
-    # @returns [String] The globally unique identifier for the experiment
+    # @return [String] The globally unique identifier for the experiment
     attr_accessor :key
 
-    # @returns [Array<String, Integer, Hash>] The different variations to choose between
+    # @return [Array<String, Integer, Hash>] The different variations to choose between
     attr_accessor :variations
 
-    # @returns [Array<Float>] How to weight traffic between variations. Must add to 1.
+    # @return [Array<Float>] How to weight traffic between variations. Must add to 1.
     attr_accessor :weights
 
-    # @returns [true, false] If set to false, always return the control (first variation)
+    # @return [true, false] If set to false, always return the control (first variation)
     attr_accessor :active
 
-    # @returns [Float] What percent of users should be included in the experiment (between 0 and 1, inclusive)
+    # @return [Float] What percent of users should be included in the experiment (between 0 and 1, inclusive)
     attr_accessor :coverage
 
-    # @returns [Array<Hash>] Array of ranges, one per variation
+    # @return [Array<Hash>] Array of ranges, one per variation
     attr_accessor :ranges
 
-    # @returns [Hash] Optional targeting condition
+    # @return [Hash] Optional targeting condition
     attr_accessor :condition
 
-    # @returns [String, nil] Adds the experiment to a namespace
+    # @return [String, nil] Adds the experiment to a namespace
     attr_accessor :namespace
 
-    # @returns [integer, nil] All users included in the experiment will be forced into the specific variation index
+    # @return [integer, nil] All users included in the experiment will be forced into the specific variation index
     attr_accessor :force
 
-    # @returns [String] What user attribute should be used to assign variations (defaults to id)
+    # @return [String] What user attribute should be used to assign variations (defaults to id)
     attr_accessor :hash_attribute
 
-    # @returns [Integer] The hash version to use (default to 1)
+    # @return [Integer] The hash version to use (default to 1)
     attr_accessor :hash_version
 
-    # @returns [Array<Hash>] Meta info about the variations
+    # @return [Array<Hash>] Meta info about the variations
     attr_accessor :meta
 
-    # @returns [Array<Hash>] Array of filters to apply
+    # @return [Array<Hash>] Array of filters to apply
     attr_accessor :filters
 
-    # @returns [String, nil] The hash seed to use
+    # @return [String, nil] The hash seed to use
     attr_accessor :seed
 
-    # @returns [String] Human-readable name for the experiment
+    # @return [String] Human-readable name for the experiment
     attr_accessor :name
 
-    # @returns [String, nil] Id of the current experiment phase
+    # @return [String, nil] Id of the current experiment phase
     attr_accessor :phase
 
     def initialize(options = {})
