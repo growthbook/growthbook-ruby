3088604e35cecf3d0f315bcd63523db4ca2ad79b
tinahollygb <tina@growthbook.io>
2023-04-26 17:25:00 -0700

    fix typo w/ casing

--- lib/growthbook/tracking_callback.rb
+++ lib/growthbook/tracking_callback.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-module GrowthBook
+module Growthbook
   # Extendable class that can be used as the tracking callback
   class TrackingCallback
     def on_experiment_viewed(_experiment, _result); end
